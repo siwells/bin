4d9a3bf78980e3b24c3779b9d4d46407d09f2dc5
Simon Wells <siwells@gmail.com>
2013-06-06 17:47:31 +0100

    JSON minifier based on python -mjson.tool script

---
+++ minj.py
@@ -0,0 +1,35 @@
+"""Command-line tool to validate and pretty-print JSON
+
+Usage::
+
+    $ echo '{"json":"obj"}' | python minj.py
+    {"json": "obj"}
+    $ echo '{ 1.2:3.4}' | python minj.py
+    Expecting property name: line 1 column 2 (char 2)
+
+"""
+import sys
+import json
+
+def main():
+    if len(sys.argv) == 1:
+        infile = sys.stdin
+        outfile = sys.stdout
+    elif len(sys.argv) == 2:
+        infile = open(sys.argv[1], 'rb')
+        outfile = sys.stdout
+    elif len(sys.argv) == 3:
+        infile = open(sys.argv[1], 'rb')
+        outfile = open(sys.argv[2], 'wb')
+    else:
+        raise SystemExit(sys.argv[0] + " [infile [outfile]]")
+    try:
+        obj = json.load(infile)
+    except ValueError, e:
+        raise SystemExit(e)
+    json.dump(obj, outfile, sort_keys=True, separators=(',',':'))
+    outfile.write('\n')
+
+
+if __name__ == '__main__':
+    main()
